c20b1778e1245acfec420d160c17306477dd6aa1
nikhiltri <ntrivedi@artic.edu>
2021-03-18 14:53:07 -0500

    Serve up data in updated schema [WEB-1998]

--- routes/api.php
+++ routes/api.php
@@ -20,28 +20,4 @@ Route::get('/', function () {
 Route::group(['prefix' => 'v1'], function() {
     Route::get('products', 'ProductController@index');
     Route::get('products/{id}', 'ProductController@show');
-
-    Route::get('categories', 'CategoryController@index');
-    Route::get('categories/{id}', 'CategoryController@show');
-
-    Route::get('colors', 'ColorController@index');
-    Route::get('colors/{id}', 'ColorController@show');
-
-    Route::get('facets', 'FacetController@index');
-    Route::get('facets/{id}', 'FacetController@show');
-
-    Route::get('keywords', 'KeywordController@index');
-    Route::get('keywords/{id}', 'KeywordController@show');
-
-    Route::get('origins', 'OriginController@index');
-    Route::get('origins/{id}', 'OriginController@show');
-
-    Route::get('stones', 'StoneController@index');
-    Route::get('stones/{id}', 'StoneController@show');
-
-    Route::get('styles', 'StyleController@index');
-    Route::get('styles/{id}', 'StyleController@show');
-
-    Route::get('artists', 'ArtistController@index');
-    Route::get('artists/{id}', 'ArtistController@show');
 });
